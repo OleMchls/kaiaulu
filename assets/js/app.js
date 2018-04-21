c7cbc521b8de647958d229ff5a9b4816e92630bb
Ole Michaelis <Ole.Michaelis@googlemail.com>
2018-04-21 08:31:21 -0700

    fix more js styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,9 +20,8 @@
 
 // import socket from "./socket"
 
+import React from 'react'
+import { render } from 'react-dom'
+import Hello from './components/Hello'
 
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './components/Hello';
-
-render(<Hello />, document.getElementById('app'));
+render(<Hello />, document.getElementById('app'))
