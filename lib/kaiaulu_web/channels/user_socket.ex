16e0f37a71eaca63244d40becee68808ac821b88
Ole Michaelis <Ole.Michaelis@googlemail.com>
2018-04-21 09:10:39 -0700

    add basic channels

--- lib/kaiaulu_web/channels/user_socket.ex
+++ lib/kaiaulu_web/channels/user_socket.ex
@@ -2,10 +2,10 @@ defmodule KaiauluWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", KaiauluWeb.RoomChannel
+  channel("room:*", KaiauluWeb.RoomChannel)
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport(:websocket, Phoenix.Transports.WebSocket)
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
