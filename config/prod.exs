12940e56ee812cc0f285ee6eb86d7ba5a833a93b
Ole Michaelis <Ole.Michaelis@googlemail.com>
2018-09-22 00:45:12 +0200

    configure unused db

--- config/prod.exs
+++ config/prod.exs
@@ -22,6 +22,15 @@ config :kaiaulu, KaiauluWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+# unused but needs to stay here
+config :kaiaulu, Kaiaulu.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "kaiaulu_dev",
+  hostname: "localhost",
+  pool_size: 10
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
